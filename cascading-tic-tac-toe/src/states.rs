987298b1a39c7130d8092b7d93b1abf326550189
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-04-24 11:11:25 -0400

    states.rs

--- cascading-tic-tac-toe/src/states.rs
+++ cascading-tic-tac-toe/src/states.rs
@@ -1,23 +1,28 @@
 use bevy::prelude::*;
 
+/// State that tracks the name of the opposing players
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States, Reflect)]
 pub enum PlayerTag {
     X,
     O,
 }
 
+/// Formatting constructor for PlayerTag
 impl std::fmt::Display for PlayerTag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
       write!(f, "{:?}", self) // Use Debug formatting for simplicity
     }
   }
 
+/// State that keeps track of current player turn
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States, Reflect)]
 pub enum PlayerTurn {
     X,
     O,
 }
 
+/// State to keep track of whether a cell is valid, invalid, filled,
+/// or part of a winning combination
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States)]
 pub enum CellState {
     Valid,
@@ -26,6 +31,8 @@ pub enum CellState {
     Won(PlayerTag), // TODO: Fill cells in winning combos with bg color (color chooser?)
 }
 
+/// State to keep track of the inner gamestate
+/// Otherwise GameState::NotPlaying is the default value
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States, Reflect, Default)]
 pub enum GameState {
     #[default]
@@ -37,6 +44,7 @@ pub enum GameState {
     Won(PlayerTag),
 }
 
+/// State to keep track of the inner round state, which is either updating or not updating
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States, Reflect, Default)]
 pub enum RoundState {
     #[default]
@@ -44,6 +52,8 @@ pub enum RoundState {
     UpdatingRound,
 }
 
+/// State to keep track of gameplay arena
+/// Online and VsComputer are not implemented
 #[derive(Debug, Clone, Eq, PartialEq, Hash, States, Reflect, Default)]
 pub enum PlayingState {
     #[default]
@@ -54,7 +64,7 @@ pub enum PlayingState {
     VsComputer, // TODO
 }
 
-// State used for the start menu screen
+/// State used for the start menu screen
 #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
 pub enum MenuState {
     #[default]
